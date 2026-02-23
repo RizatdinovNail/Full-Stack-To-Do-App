27e38002d0a56933a7fceee44e6fbf0c6cb768b4
Nail Rizatdinov <rizatdinovnail.job@gmail.com>
2026-02-23 14:25:36 +0100

    feat(backend): fixed sever.js file

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.get("/", (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/uploads", express.static("uploads"));
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
